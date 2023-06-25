ef7cbc654e7671ea6dc0457f58ee0fa0886851d7
kunal00000 <kunalverma2468@gmail.com>
2023-06-26 02:09:26 +0530

    initial fetching functions

--- todo-frontend/src/App.tsx
+++ todo-frontend/src/App.tsx
@@ -1,35 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+
+  function getAllTodos() {
+    fetch("http://localhost:3000/todos", {
+      method: "GET",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setTodos(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        return error;
+      });
+  }
+
+  function postTodos() {
+    const title = (document.getElementById("title") as HTMLInputElement).value;
+    const description = (
+      document.getElementById("description") as HTMLInputElement
+    ).value;
+    const completed = (document.getElementById("completed") as HTMLInputElement)
+      .value;
+
+    fetch("http://localhost:3000/todos", {
+      method: "POST",
+      body: JSON.stringify({
+        title: title,
+        description: description,
+        completed: completed,
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        getAllTodos();
+      })
+      .catch((error) => {
+        console.log(error);
+        return error;
+      });
+  }
+
+  function deleteTodo(id: number) {
+    fetch(`http://localhost:3000/todos/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        console.log(response);
+        getAllTodos();
+      })
+      .catch((error) => {
+        console.log(error);
+        return error;
+      });
+  }
+
+  useEffect(() => {
+    getAllTodos();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="card">
+      <div className="card-header border-black">Todo List</div>
+      <div className="card-body">
+        <ul className="list-group">
+          {todos.map((todo: any) => (
+            <li className="list-group-item" key={todo.id}>
+              {todo.title}
+              {todo.completed ? (
+                <span className="badge bg-success rounded-pill float-end">
+                  Completed
+                </span>
+              ) : (
+                <span className="badge bg-danger rounded-pill float-end">
+                  Pending
+                </span>
+              )}
+              {todo.description && <p className="mt-2">{todo.description}</p>}
+              <button
+                onClick={() => {
+                  deleteTodo(todo.id);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <input type="text" id="title" />
+      <input type="text" id="description" />
+      <select id="completed">
+        <option value="true">Completed</option>
+        <option value="false">Pending</option>
+      </select>
+
+      <button onClick={postTodos}>Post Todo</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
