1508669a9fcb9ec5134b04f818c657bff9093260
kunal00000 <kunalverma2468@gmail.com>
2023-06-27 02:45:58 +0530

    feat: react-query setup for data-fetching

--- todo-frontend/src/App.tsx
+++ todo-frontend/src/App.tsx
@@ -1,106 +1,37 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import React from "react";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import "./styles/App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  async function getAllTodos() {
-    try {
-      const response = await fetch("http://localhost:3000/todos", {
-        method: "GET",
-      });
-      const data = await response.json();
-      setTodos(data);
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  async function postTodos() {
-    try {
-      const title = (document.getElementById("title") as HTMLInputElement)
-        .value;
-      const description = (
-        document.getElementById("description") as HTMLInputElement
-      ).value;
-      const completed = (
-        document.getElementById("completed") as HTMLInputElement
-      ).value;
-
-      const response = await fetch("http://localhost:3000/todos", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          title: title,
-          description: description,
-          completed: completed,
-        }),
-      });
-      const data = await response.json();
-      console.log(data);
-      getAllTodos();
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  async function deleteTodo(id: number) {
-    try {
-      const response = await fetch(`http://localhost:3000/todos/${id}`, {
-        method: "DELETE",
-      });
-      const data = await response.text();
-      console.log(data);
-      getAllTodos();
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  useEffect(() => {
-    getAllTodos();
-  }, []);
-
   return (
-    <div className="card">
-      <div className="card-header border-black">Todo List</div>
-      <div className="card-body">
-        <ul className="list-group">
-          {todos.map((todo: any) => (
-            <li className="list-group-item" key={todo.id}>
-              {todo.title}
-              {todo.completed ? (
-                <span className="badge bg-success rounded-pill float-end">
-                  Completed
-                </span>
-              ) : (
-                <span className="badge bg-danger rounded-pill float-end">
-                  Pending
-                </span>
-              )}
-              {todo.description && <p className="mt-2">{todo.description}</p>}
-              <button
-                onClick={() => {
-                  deleteTodo(todo.id);
-                }}
-              >
-                Delete
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <input type="text" id="title" />
-      <input type="text" id="description" />
-      <select id="completed">
-        <option value="true">Completed</option>
-        <option value="false">Pending</option>
-      </select>
-
-      <button onClick={postTodos}>Post Todo</button>
-    </div>
+    <Routes>
+      <Route
+        path="/todos"
+        element={
+          <React.Fragment>
+            <TodoForm />
+          </React.Fragment>
+        }
+      />
+      <Route
+        path="/todo"
+        element={
+          <React.Fragment>
+            <TodoList />
+          </React.Fragment>
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <React.Fragment>
+            <TodoForm />
+          </React.Fragment>
+        }
+      />
+    </Routes>
   );
 }
 
--- todo-frontend/src/main.tsx
+++ todo-frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
+    <Router>
+      <App />
+    </Router>
+  </QueryClientProvider>
+);
